c12250119b51b7eecab542ecc9e1ae2abff3635f
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-04-06 02:36:50 -0700

    fix

--- img.js
+++ img.js
@@ -1,42 +1,4 @@
 
-// Filtering function
-function filterMaterialsForCharacter(materials, characterName) {
-    const filteredMaterials = [];
-    for (const materialKey in materials) {
-        if (materials.hasOwnProperty(materialKey)) {
-            const material = materials[materialKey];
-            if (material.characters && material.characters.includes(characterName)) {
-                // filteredMaterials[materialKey] = material;
-                filteredMaterials.push({
-                    indexName: materialKey,
-                    materialName: material.name
-                })
-            }
-        }
-    }
-    return filteredMaterials;
-}
-
-// Filtering function
-function filterRegionalMaterialsForCharacter(data, characterName) {
-    const filteredMaterials = [];
-
-    for (const region in data) {
-        data[region].forEach(material => {
-            if (material.characters.includes(characterName)) {
-                // filteredMaterials[materialKey] = material;
-                filteredMaterials.push({
-                    indexName: region,
-                    materialID: material.id,
-                    materialName: material.name})
-                }
-            });
-        }
-    return filteredMaterials;
-}
-
-
-
 // Putting everything into images
 async function init(input) {
     var character_name = input.value.toLowerCase();
