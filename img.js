0cc69c1cf3f4a377264ee137d5685633f299a68c
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-04-06 00:54:01 -0700

    hopefully it works

--- img.js
+++ img.js
@@ -4,11 +4,20 @@ function init(input) {
 
     if(character_name != null ){
         let url = 'https://genshin.jmp.blue/characters/' + character_name ;
+        let msg = "Materials for" + character_name + ":";
     
         fetch(url)
         .then(res => res.json())
         .then(data => {
             document.getElementById('charimg').src = url + "/card";
+
+        // Filter data for "Venti"
+        const bossmats = data.filter(material => material.characters.includes(character_name));
+
+        // Output filtered materials
+        console.log(msg, bossmats);
+    })
+    .catch(error => console.error('Error fetching data:', error));
           })
     
     }
