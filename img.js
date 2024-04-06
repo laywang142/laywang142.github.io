90863906618a99fb8567b2b17a5eefeba865c762
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-04-06 02:06:19 -0700

    reorganize

--- img.js
+++ img.js
@@ -17,6 +17,28 @@ function filterMaterialsForCharacter(materials, characterName) {
     return filteredMaterials;
 }
 
+// Filtering function
+function filterRegionalMaterialsForCharacter(data, characterName) {
+    const filteredMaterials = [];
+
+    for (const region in data) {
+        if (data.hasOwnProperty(region)) {
+            const materials = data[region];
+            for (const material of materials) {
+                if (material.characters.includes(characterName)) {
+                // filteredMaterials[materialKey] = material;
+                filteredMaterials.push({
+                    indexName: region,
+                    materialID: material.id,
+                    materialName: material.name
+                })
+            }
+        }
+    }
+    return filteredMaterials;
+}
+}
+
 
 // Putting everything into images
 async function init(input) {
@@ -25,32 +47,33 @@ async function init(input) {
     // boss materials
     const response = await fetch('https://genshin.jmp.blue/materials/boss-material');
     const bossmat = await response.json();
-    //console.log(bossmat)
 
-    // Filter data for character
-    const boss = filterMaterialsForCharacter(bossmat, character_name);
-    //console.log(boss.map(item => item.indexName))
+    // regional specialities
+    const regional = await (await fetch('https://genshin.jmp.blue/materials/local-specialties')).json()
+
+    // ascesion materials
+    const ascension = await (await fetch('https://genshin.jmp.blue/materials/character-ascension')).json();
+
+    // common ascension materials 
+    const mats = await (await fetch('https://genshin.jmp.blue/materials/common-ascension')).json();
 
     if(character_name != null ){
         let url = 'https://genshin.jmp.blue/characters/' + character_name ;
-        let msg = "Materials for" + character_name + ":";
+        const boss = filterMaterialsForCharacter(bossmat, character_name);
+        const reg = filterRegionalMaterialsForCharacter(regional, character_name);
     
         fetch(url)
         .then(res => res.json())
         .then(data => {
             document.getElementById('charimg').src = url + "/card";
             document.getElementById('bossmat').src = 'https://genshin.jmp.blue/materials/boss-material/' + boss.map(item => item.indexName);
+            document.getElementById('regimg').src = 'https://genshin.jmp.blue/materials/local-specialties/' + reg.map(item => item.materialID);
     })
     .catch(error => console.error('Error fetching data:', error));
 
 
       }  
         
-//   // Listen for when the input changes.
-//   input.addEventListener('change', onInputChange)
-//   function onInputChange(e) {
-//     reader.readAsDataURL(this.files[0]);
-//   }
 }
 
 
