39560fb6be335e433dc3db2cdc85bcbafa1ed40a
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-04-21 10:30:34 -0700

    added nouislider

--- helpers.js
+++ helpers.js
@@ -1,3 +1,60 @@
+// create slider 1
+var slider = document.getElementById('slider-1');
+
+noUiSlider.create(slider, {
+    start: [1, 8],
+    connect: true,
+    range: {
+        'min': 1,
+        'max': 10
+    },
+
+    // Show a scale with the slider
+    pips: {
+      mode: 'steps',
+      stepped: true,
+      density: 10
+  }
+});
+
+// create slider 2
+var slider = document.getElementById('slider-2');
+
+noUiSlider.create(slider, {
+    start: [1, 8],
+    connect: true,
+    range: {
+        'min': 1,
+        'max': 10
+    },
+
+    // Show a scale with the slider
+    pips: {
+      mode: 'steps',
+      stepped: true,
+      density: 10
+  }
+});
+
+// create slider 3
+var slider = document.getElementById('slider-3');
+
+noUiSlider.create(slider, {
+    start: [1, 8],
+    connect: true,
+    range: {
+        'min': 1,
+        'max': 10
+    },
+
+    // Show a scale with the slider
+    pips: {
+      mode: 'steps',
+      stepped: true,
+      density: 10
+  }
+});
+
 // Getting options for select field 
 
 $(document).ready(function () {
